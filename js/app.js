b2247dc3a42013c9f241d88cff40219fea32a253
unknown <matthias.cartel@gmail.com>
2019-11-30 14:51:12 +0100

    add features -> Click on layers : display markers, need to fix the openPopup function"

--- js/app.js
+++ js/app.js
@@ -3,8 +3,24 @@ new Vue({
 
     data: {
         map: null,
-        titleLayer: null, // Initialisation des données : null et vide pour les calques
-        layers: [],
+        titleLayer: null, // Initialisation des données : null titres des calques
+        layers: [
+            {
+                id: 0,
+                name: 'Porte de Champeret',
+                type: 'marker',
+                coords: [48.885495, 2.293116],
+                active: false,
+            },
+            {
+                id:1,
+                name: 'Notre Dame',
+                type:'marker',
+                coords: [48.852810, 2.350384],
+                active: false,
+            },
+            
+        ],
     },
 
     mounted() {
@@ -14,7 +30,7 @@ new Vue({
 
     methods: {
         initMap() {
-            this.map = L.map('map').setView([50.633333, 3.066667], 12);
+            this.map = L.map('map').setView([48.856697, 2.351462], 12);
             this.tileLayer = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/voyager/{z}/{x}/{y}.png',
                     {
                         maxZoom: 18,
@@ -24,6 +40,22 @@ new Vue({
             this.tileLayer.addTo(this.map);
         },
 
-        initLayers() {}, // méthodes exécutées
+        initLayers() {
+            this.layers.forEach((layer) =>
+            {
+                layer.leafletObject = L.marker(layer.coords).bindPopup(layer.name).openPopup();
+            });
+        },// méthodes exécutées 
+        layerChanged(layerId,active) {
+            const layer = this.layers.find(layer => layer.id === layerId);
+                if (active) 
+                {
+                    layer.leafletObject.addTo(this.map);
+                } 
+                else 
+                {
+                    layer.leafletObject.removeFrom(this.map);
+                }
+        }, 
     },
 });
